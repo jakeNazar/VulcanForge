e7b8f3094d4f12fdf7fdd6199ef0d159a777c907
jakeNazar <jake.nazario@tutamail.com>
2025-08-08 10:51:41 +0330

    Added operators.py

---
+++ basics/operators.py
@@ -0,0 +1,162 @@
+#Arithmetic operators:
+a = 21
+b = 10
+c = 0
+
+c = a + b
+print ("a: {} b: {} a+b: {}".format(a,b,c))
+
+c = a - b
+print ("a: {} b: {} a-b: {}".format(a,b,c) )
+
+c = a * b
+print ("a: {} b: {} a*b: {}".format(a,b,c))
+
+c = a / b
+print ("a: {} b: {} a/b: {}".format(a,b,c))
+
+c = a % b
+print ("a: {} b: {} a%b: {}".format(a,b,c))
+
+a = 2
+b = 3
+c = a**b 
+print ("a: {} b: {} a**b: {}".format(a,b,c))
+
+a = 10
+b = 5
+c = a//b 
+print ("a: {} b: {} a//b: {}".format(a,b,c))
+
+#Comparision operators:
+a = 21
+b = 10
+if ( a == b ):
+   print ("Line 1 - a is equal to b")
+else:
+   print ("Line 1 - a is not equal to b")
+
+if ( a != b ):
+   print ("Line 2 - a is not equal to b")
+else:
+   print ("Line 2 - a is equal to b")
+
+if ( a < b ):
+   print ("Line 3 - a is less than b" )
+else:
+   print ("Line 3 - a is not less than b")
+
+if ( a > b ):
+   print ("Line 4 - a is greater than b")
+else:
+   print ("Line 4 - a is not greater than b")
+
+a,b=b,a #values of a and b swapped. a becomes 10, b becomes 21
+
+if ( a <= b ):
+   print ("Line 5 - a is either less than or equal to  b")
+else:
+   print ("Line 5 - a is neither less than nor equal to  b")
+
+if ( b >= a ):
+   print ("Line 6 - b is either greater than  or equal to b")
+else:
+   print ("Line 6 - b is neither greater than  nor equal to b")
+
+#Assignment operators:
+a = 21
+b = 10
+c = 0
+print ("a: {} b: {} c : {}".format(a,b,c))
+c = a + b
+print ("a: {}  c = a + b: {}".format(a,c))
+
+c += a
+print ("a: {} c += a: {}".format(a,c))
+
+c *= a
+print ("a: {} c *= a: {}".format(a,c))
+
+c /= a 
+print ("a: {} c /= a : {}".format(a,c))
+
+c  = 2
+print ("a: {} b: {} c : {}".format(a,b,c))
+c %= a
+print ("a: {} c %= a: {}".format(a,c))
+
+c **= a
+print ("a: {} c **= a: {}".format(a,c))
+
+c //= a
+print ("a: {} c //= a: {}".format(a,c))
+
+#Bitwise operators:
+a = 20            
+b = 10            
+
+print ('a=',a,':',bin(a),'b=',b,':',bin(b))
+c = 0
+
+c = a & b;        
+print ("result of AND is ", c,':',bin(c))
+
+c = a | b;     
+print ("result of OR is ", c,':',bin(c))
+
+c = a ^ b;        
+print ("result of EXOR is ", c,':',bin(c))
+
+c = ~a;           
+print ("result of COMPLEMENT is ", c,':',bin(c))
+
+c = a << 2;       
+print ("result of LEFT SHIFT is ", c,':',bin(c))
+
+c = a >> 2;       
+print ("result of RIGHT SHIFT is ", c,':',bin(c))
+
+#Logical operators:
+var = 5
+
+print(var > 3 and var < 10)
+print(var > 3 or var < 4)
+print(not (var > 3 and var < 10))
+
+#Membership operators:
+'''
+    Determines if it is a member of a sequence (list, string or tuple)
+'''
+a = 10
+b = 20
+list = [1, 2, 3, 4, 5 ]
+
+print ("a:", a, "b:", b, "list:", list)
+
+if ( a in list ):
+   print ("a is present in the given list")
+else:
+   print ("a is not present in the given list")
+
+if ( b not in list ):
+   print ("b is not present in the given list")
+else:
+   print ("b is present in the given list")
+
+c=b/a
+print ("c:", c, "list:", list)
+if ( c in list ):
+   print ("c is available in the given list")
+else:
+    print ("c is not available in the given list")
+
+#Identity operators:
+a = [1, 2, 3, 4, 5]
+b = [1, 2, 3, 4, 5]
+c = a
+
+print(a is c)
+print(a is b)
+
+print(a is not c)
+print(a is not b)
